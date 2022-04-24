12724d88a62ad7636e98de694353f4bc1d6f87e3
fanxiaoyu <fan-xy19@mails.tsinghua.edu.cn>
2022-04-24 21:26:29 +0800

    3

--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -33,12 +33,13 @@ pub fn sys_yield() -> isize {
 // YOUR JOB: 引入虚地址后重写 sys_get_time
 pub fn sys_get_time(_ts: *mut TimeVal, _tz: usize) -> isize {
     let _us = get_time_us();
-    // unsafe {
-    //     *ts = TimeVal {
-    //         sec: us / 1_000_000,
-    //         usec: us % 1_000_000,
-    //     };
-    // }
+    // println!("{}",_us);
+    unsafe {
+        *_ts = TimeVal {
+            sec: _us / 1_000_000,
+            usec: _us % 1_000_000,
+        };
+    }
     0
 }
 
